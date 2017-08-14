31c396f49f39e64f1e41dac864d0f3f8ef2fbffe
Akira <ngokakr@gmail.com>
2017-08-14 08:29:19 +0000

    kadai

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
+  resources :tasks
+  # get "tasks/:id" , to: "tasks#show"
+  # post "tasks" , to: "tasks#create"
+  # put "tasks/:id" , to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  
+  # get "tasks" , to: "tasks#create"
+  
+  # get "tasks/new" , to: "tasks#new"
+  
+  # put "tasks/:id" ,to: "tasks#update"
+  
+  
+  
 end
